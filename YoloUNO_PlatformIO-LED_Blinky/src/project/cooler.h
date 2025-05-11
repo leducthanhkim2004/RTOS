74aa798789eee7bf8d566a40514daf6edf6e3ee5
leducthanhkim2004 <10422105@student.vgu.edu.vn>
2025-05-11 12:10:51 +0700

    add_cooler.h

--- YoloUNO_PlatformIO-LED_Blinky/src/project/cooler.h
+++ YoloUNO_PlatformIO-LED_Blinky/src/project/cooler.h
@@ -4,17 +4,19 @@
 #ifndef COOLER_H
 #define COOLER_H
 
-#include <Arduino.h>  
+#include <Arduino.h>  // For digitalWrite and pin definitions
 
-
+// Pin definitions for Cooler LED
 #define D5 8  // Cooler LED (bit 0)
 #define D6 9  // Cooler LED (bit 1)
-#define INIT 0
-#define CHECK_STATE 1
+#define INIT 0;
+#define COOLER_OFF 1;
+#define COOLER_ON 2;
 extern float cooler_threshold;  
 
 void cooler_task();
-
+void cooler_on();
+void cooler_off();
 
 
 #endif
